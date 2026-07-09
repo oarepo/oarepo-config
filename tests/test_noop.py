c7373e1e9113ad1e17e81ae52ead3eaca13b5252
Mirek Simek <miroslav.simek@cesnet.cz>
2026-07-09 09:52:08 +0200

    tests

--- tests/test_noop.py
+++ tests/test_noop.py
@@ -1,2 +1,15 @@
+#
+# Copyright (c) 2026 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-config (see https://github.com/oarepo/oarepo-config).
+#
+# oarepo-config is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""A no-op test for this moment."""
+
+from __future__ import annotations
+
+
 def test_noop():
     pass
